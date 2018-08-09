f93c8015670e7df0e8e18ed21b5d914b42cd496b
bauassr <bauassr@gmail.com>
2018-08-09 22:07:53 +0530

    Add files via upload

---
+++ session24.py
@@ -0,0 +1,11 @@
+import sqlite3
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import matplotlib.pyplot as plt
+
+
+ 
